3696339f9706efcf7108e0f2019a4a4c7e8e4ada
Abel Moremi <abelmoremiz@gmail.com>
2026-09-05 19:22:20 +0200

    remove: vanila js implementation

--- app.js
+++
@@ -1,9 +0,0 @@
-const themeToggle = document.getElementById('theme-toggle');
-
-if (themeToggle) {
-  themeToggle.addEventListener('click', () => {
-    const root = document.documentElement;
-    const isDark = root.getAttribute('data-theme') === 'dark';
-    root.setAttribute('data-theme', isDark ? 'light' : 'dark');
-  });
-}
